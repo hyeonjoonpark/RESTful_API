9506e568563d66c3a095bf584dd6c384f2f9e61f
hyeonjoonpark <pjjoon1379@gmail.com>
2023-02-09 21:58:56 +0900

    ADD : API

--- src/main.js
+++ src/main.js
@@ -12,6 +12,27 @@
 const http = require("http");
 
 /**
+ * @typedef Post
+ * @property {string} id
+ * @property {string} title
+ * @property {string} content
+ */
+
+/** @type {Post[]} */
+const posts = [
+  {
+    id: "my_first_post",
+    title: "My first post",
+    content: "Hello",
+  },
+  {
+    id: "my_second_post",
+    title: "나의 두번째 포스트",
+    content: "Second post",
+  },
+];
+
+/**
  * Post
  *
  * GET /posts
@@ -21,16 +42,55 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   const POST_ID_REGEX = /^\/posts\/([a-zA-Z0-9-_]+)$/;
+  const postIdRegexResult =
+    (req.url && POST_ID_REGEX.exec(req.url)) || undefined;
 
   if (req.url === "/posts" && req.method === "GET") {
+    const result = {
+      posts: posts.map((post) => ({
+        id: post.id,
+        title: post.title,
+      })),
+      totalCount: posts.length,
+    };
+
     res.statusCode = 200;
-    res.end("List of posts");
-  } else if (req.url && POST_ID_REGEX.test(req.url)) {
-    const regexResult = POST_ID_REGEX.exec(req.url);
-    console.log(regexResult);
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    res.end(JSON.stringify(result));
+  } else if (postIdRegexResult && req.method === "GET") {
+    // GET /posts/:id
+    const postId = postIdRegexResult[1];
+    const post = posts.find((_post) => _post.id === postId);
+
+    if (post) {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json; charset=utf-8");
+      res.end(JSON.stringify(post));
+    } else {
+      res.statusCode = 404;
+      res.end("Post not found");
+    }
+
     res.statusCode = 200;
-    res.end("Some content of the post");
+    res.end("Reading a post");
   } else if (req.url === "/posts" && req.method === "POST") {
+    req.setEncoding("utf-8");
+    req.on("data", (data) => {
+      /** @typedef CreatePostBody
+       * @property {string} title
+       * @property {string} content
+       */
+
+      /** @type {CreatePostBody} */
+      const body = JSON.parse(data);
+      console.log(body);
+      posts.push({
+        id: body.title.toLowerCase().replace(/\s/g, "_"),
+        title: body.title,
+        content: body.content,
+      });
+    });
+
     res.statusCode = 200;
     res.end("Creating post");
   } else {
