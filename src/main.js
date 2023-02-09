b59ace3c41bea4f80daaacf251c77534dd805424
hyeonjoonpark <pjjoon1379@gmail.com>
2023-02-10 00:43:10 +0900

    ADD : Refectoring

--- src/main.js
+++ src/main.js
@@ -11,91 +11,25 @@
 
 const http = require("http");
 
-/**
- * @typedef Post
- * @property {string} id
- * @property {string} title
- * @property {string} content
- */
-
-/** @type {Post[]} */
-const posts = [
-  {
-    id: "my_first_post",
-    title: "My first post",
-    content: "Hello",
-  },
-  {
-    id: "my_second_post",
-    title: "나의 두번째 포스트",
-    content: "Second post",
-  },
-];
-
-/**
- * Post
- *
- * GET /posts
- * GET /posts/:id
- * POST /posts
- */
-
+const { routes } = require("./api");
 const server = http.createServer((req, res) => {
-  const POST_ID_REGEX = /^\/posts\/([a-zA-Z0-9-_]+)$/;
-  const postIdRegexResult =
-    (req.url && POST_ID_REGEX.exec(req.url)) || undefined;
+  async function main() {
+    const route = routes.find(
+      (_route) =>
+        req.url &&
+        req.method &&
+        _route.url.test(req.url) &&
+        _route.method === req.method
+    );
 
-  if (req.url === "/posts" && req.method === "GET") {
-    const result = {
-      posts: posts.map((post) => ({
-        id: post.id,
-        title: post.title,
-      })),
-      totalCount: posts.length,
-    };
-
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json; charset=utf-8");
-    res.end(JSON.stringify(result));
-  } else if (postIdRegexResult && req.method === "GET") {
-    // GET /posts/:id
-    const postId = postIdRegexResult[1];
-    const post = posts.find((_post) => _post.id === postId);
-
-    if (post) {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json; charset=utf-8");
-      res.end(JSON.stringify(post));
-    } else {
-      res.statusCode = 404;
-      res.end("Post not found");
+    if (!route) {
+      (res.statusCode = 404), res.end("Not found");
+      return;
     }
 
-    res.statusCode = 200;
-    res.end("Reading a post");
-  } else if (req.url === "/posts" && req.method === "POST") {
-    req.setEncoding("utf-8");
-    req.on("data", (data) => {
-      /** @typedef CreatePostBody
-       * @property {string} title
-       * @property {string} content
-       */
-
-      /** @type {CreatePostBody} */
-      const body = JSON.parse(data);
-      console.log(body);
-      posts.push({
-        id: body.title.toLowerCase().replace(/\s/g, "_"),
-        title: body.title,
-        content: body.content,
-      });
-    });
-
-    res.statusCode = 200;
-    res.end("Creating post");
-  } else {
-    res.statusCode = 404;
-    res.end("Not found");
+    const result = await route.callback();
+    res.statusCode = result.statusCode;
+    res.end(result.body);
   }
 });
 
