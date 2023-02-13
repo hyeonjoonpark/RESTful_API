e06a6d7d3fa84a82f8e5516c939c6e4152ef5579
hyeonjoonpark <pjjoon1379@gmail.com>
2023-02-13 21:56:28 +0900

    ADD : Error fix

--- src/api.js
+++ src/api.js
@@ -10,16 +10,16 @@
 /** @type {Post[]} */
 const posts = [
   {
-    id: "my_first_post",
-    title: "My first post",
-    content: "Hello",
+    id: 'my_first_post',
+    title: 'My first post',
+    content: 'Hello',
   },
   {
-    id: "my_second_post",
-    title: "나의 두번째 포스트",
-    content: "Second post",
+    id: 'my_second_post',
+    title: '나의 두번째 포스트',
+    content: 'Second post',
   },
-];
+]
 
 /**
  * Post
@@ -46,7 +46,7 @@ const posts = [
  * @typedef CreatePostBody
  * @property {string} title
  * @property {string} content
-*/
+ */
 
 /** @type {CreatePostBody} */
 
@@ -54,7 +54,7 @@ const posts = [
 const routes = [
   {
     url: /^\/posts$/,
-    method: "GET",
+    method: 'GET',
     callback: async () => ({
       // TODO: implement
       statusCode: 200,
@@ -64,60 +64,60 @@ const routes = [
 
   {
     url: /^\/posts\/([a-zA-Z0-9-_]+)$/,
-    method: "GET",
+    method: 'GET',
     callback: async (matches) => {
-      const postId = matches[1];
+      const postId = matches[1]
       if (!postId) {
         return {
           statusCode: 404,
-          body: "Not found",
-        };
+          body: 'Not found',
+        }
       }
 
-      const post = posts.find((_post) => _post.id === postId);
+      const post = posts.find((_post) => _post.id === postId)
       if (!post) {
         return {
           statusCode: 404,
-          body: "Not found",
-        };
+          body: 'Not found',
+        }
       }
 
       return {
         statusCode: 200,
         body: post,
-      };
+      }
     },
   },
 
   {
     url: /^\/posts$/,
-    method: "POST",
-    callback: async (_, body) => ({
-        if(!body) {
-            return {
-                statusCode: 400,
-                body: 'Ill-formed request',
-            }
-        }
-        /** @type {string} */
-        /* eslint-disable-next-line prefer-destrictiring */,
-        const :title = body.title,
-        const :newPost = {
-            id: title.replace(/\s/g, '_'),
-            title,
-            content: body.content,
-        },
-
-        posts.push(newPost),
-
+    method: 'POST',
+    callback: async (_, body) => {
+      if (!body) {
         return {
-            statusCode: 200,
-            body: newPost,
+          statusCode: 400,
+          body: 'Ill-formed request',
         }
-        }),
+      }
+      /** @type {string} */
+      /* eslint-disable-next-line prefer-destrictiring */
+      const title = body.title
+      const newPost = {
+        id: title.replace(/\s/g, '_'),
+        title,
+        content: body.content,
+      }
+
+      posts.push(newPost)
+
+      return {
+        statusCode: 200,
+        body: newPost,
+      }
+    },
   },
-];
+]
 
 module.exports = {
   routes,
-};
+}
